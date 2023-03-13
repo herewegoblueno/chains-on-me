8dca601666882079f35cef08f02061f51604193b
Anderson <34724755+andersonaddo@users.noreply.github.com>
2023-03-13 12:18:16 -0400

    Cleaned up some of the scripts just cuz

--- local_compile.sh
+++ local_compile.sh
@@ -1,7 +1,5 @@
 # This is designed to work on my local mac, not really the department machines
 
-input="input/7_14.sched"
-
 classfilefolder="compiled"
 jarpath="/Applications/CPLEX_Studio2211/cpoptimizer/lib/ILOG.CP.jar"
 
--- local_run.sh
+++ local_run.sh
@@ -4,7 +4,6 @@ classfilefolder="compiled"
 jarpath="/Applications/CPLEX_Studio2211/cpoptimizer/lib/ILOG.CP.jar"
 nativecode="/Applications/CPLEX_Studio2211/opl/bin/x86-64_osx"
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$nativecode1:$nativecode2:$nativecode3
 #Can't use DYLD_LIBRARY_PATH because of Appple SIP (I think?); will use -Djava.library.path instead
 #https://developer.apple.com/forums/thread/703757
 java -Djava.library.path=$nativecode -cp $jarpath:$classfilefolder solver.cp.Main $1
